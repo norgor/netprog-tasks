c73acdcb96122d8f51207e9b9bf2b451428fce8a
norgor <7034394+norgor@users.noreply.github.com>
2022-01-12 18:46:03 +0100

    improved performance by using heaps

--- p1/src/main.rs
+++ p1/src/main.rs
@@ -1,31 +1,45 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 use std::env;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, RecvError, Sender};
 use std::thread;
 
 fn is_prime(v: u32) -> bool {
     if v > 1 {
-        let v_sqrt = (v as f64).sqrt() as u32;
-        (2..v_sqrt).any(|i| v % i == 0)
+        let v_sqrt = (v as f64).sqrt() as u32 + 1;
+        !(2..v_sqrt).any(|x| v % x == 0)
     } else {
         false
     }
 }
 
-fn find_primes(start: u32, end: u32, step: u32) -> Vec<u32> {
+fn find_primes(start: u32, end: u32, step: u32, tx: Sender<u32>) {
     (start..=end)
         .step_by(step as usize)
         .filter(|v| is_prime(*v))
-        .collect()
+        .for_each(|v| tx.send(v).unwrap());
+    drop(tx);
 }
 
-fn find_primes_in_range(start: u32, end: u32, threads: u32) -> Vec<u32> {
-    let mut primes: Vec<u32> = (0..threads)
-        .map(|i| thread::spawn(move || find_primes(start + i, end, threads)))
-        .collect::<Vec<thread::JoinHandle<Vec<u32>>>>()
-        .into_iter()
-        .flat_map(|x| x.join().unwrap())
+fn find_primes_in_range(start: u32, end: u32, threads: u32) -> BinaryHeap<Reverse<u32>> {
+    let (tx, rx): (Sender<u32>, Receiver<u32>) = mpsc::channel();
+    let handles: Vec<thread::JoinHandle<()>> = (0..threads)
+        .map(|x| (x, tx.clone()))
+        .map(|x| thread::spawn(move || find_primes(start + x.0, end, threads, x.1)))
         .collect();
-    primes.sort();
-    primes
+    drop(tx);
+    let mut heap = BinaryHeap::new();
+
+    loop {
+        let result = rx.recv();
+        if result == Err(RecvError) {
+            break;
+        }
+        heap.push(Reverse(result.unwrap()));
+    }
+    handles.into_iter().for_each(|x| x.join().unwrap());
+    heap
 }
 
 fn main() {
@@ -39,6 +53,11 @@ fn main() {
         start, end, threads
     );
 
-    let primes = find_primes_in_range(start, end, threads);
-    println!("Primes: {:?}", primes);
+    let mut primes = find_primes_in_range(start, end, threads);
+    print!("Primes: ");
+    while primes.len() > 0 {
+        let Reverse(p) = primes.pop().unwrap();
+        print!("{} ", p);
+    }
+    println!("");
 }
